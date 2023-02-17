ded90b1ab151838bebf2ba17bd753dd9e029807f
faizan <faizansohail077@gmail.com>
2023-02-17 15:15:24 +0500

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,63 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black' >
+      <div className='px-5 xl:px-0 flex flex-wrap md:flex-nowrap justify-between py-10 text-white md:mt-20 mx-auto md:max-w-7xl font-jose' >
+
+        <div className='w-full mt-10 md:mt-0 ' >
+          <p className='font-bold' >About Shop</p>
+
+          <ul className='list-none' >
+            <li className='footer-list' >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</li>
+            <li className='footer-list' >E. karubar@gmail.com</li>
+            <li className='footer-list' >T. 004434424</li>
+          </ul>
+        </div>
+
+        <div className='w-full mt-10 md:mt-0 md:text-right'>
+          <p>Company</p>
+          <ul className='list-none' >
+            <li className='footer-list' >About us</li>
+            <li className='footer-list' >Accounts</li>
+            <li className='footer-list' >Contact us</li>
+            <li className='footer-list' >Order Tracking</li>
+            <li className='footer-list' >News</li>
+          </ul>
+        </div>
+
+        <div className='w-full mt-10 md:mt-0 md:text-right'>
+          <p>Customer Care</p>
+          <ul className='list-none' >
+            <li className='footer-list' >Terms & Condition</li>
+            <li className='footer-list' >Shipping Policy</li>
+            <li className='footer-list' >FAQs</li>
+            <li className='footer-list' >Refund Policy</li>
+            <li className='footer-list' >Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div className='w-full mt-10 md:mt-0 md:text-right'>
+
+          <p>Follow us</p>
+          <ul className='list-none' >
+            <li className='footer-list' >Facebook</li>
+            <li className='footer-list' >Instagram</li>
+            <li className='footer-list' >Linkedin</li>
+            <li className='footer-list' >Youtube</li>
+            <li className='footer-list' >Twitter</li>
+          </ul>
+        </div>
+
+      </div>
+     
+      <div className="mx-auto md:max-w-7xl  border-t-2 border-white py-5 text-white">
+        <div className="flex items-center justify-between ">
+          <p>@2022 www.karobarhq.com  all right reserved</p>
+          <p>Privacy Policy & Term of Use</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
